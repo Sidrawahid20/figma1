ebfc6591405419554d6700f810b44e81542d28fe
Sidrawahid20 <167582171+Sidrawahid20@users.noreply.github.com>
2024-12-09 23:13:42 +0800

    initiat

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+  <main>
+{/* Hero Section */}
+<section>
+  {/* Left side */}
+  <div>
+    <h1 className="font-bold-3xl">Hi, i'm sidra khan</h1>
+    <p>I'm a devrloper with passion for building beautifull websites and applications.
+     </p>
+    <button>View my work</button>
+  </div>
+
+      {/* Right side */} 
+<div>
+  <img width="200 px" height="400px" src="/Poster.jpg" alt="shoes" />
+</div>
+</section>
+  </main>  
+  );
+}
